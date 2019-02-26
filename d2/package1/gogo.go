f1b36a474a0081ca3cebc24e5955207a350d4fbb
dayu <jierli@gmail.com>
2019-02-26 21:05:16 +0800

    d2

---
+++ d2/package1/gogo.go
@@ -0,0 +1,5 @@
+package package1
+
+func Addttt() string {
+	return "a + b + gogo"
+}
---
+++ d2/thirdlib/testpack.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dayugolang/d2/golib"
+	"github.com/dayugolang/d2/package1"
+)
+
+func main() {
+	fmt.Println(golib.Add(7, 2))
+	fmt.Println(package1.Addttt())
+}
