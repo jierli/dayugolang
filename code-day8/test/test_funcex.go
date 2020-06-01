e8a3deb639dbbc3ffe08902a80b251e00f30f7ea
李经宇 <jingyu.li@ucarinc.com>
2020-06-01 19:59:46 +0800

    202006012

---
+++ code-day8/test/test_funcex.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type user struct {
+	name string
+	age	int
+	addr string
+}
+
+func (user user)getuser(name string)  {
+	fmt.Println(user.name)
+}
+
+func main()  {
+	u:=user{
+		"dayu",
+		34,
+		"xiameng",
+	}
+	get1:=(*user).getuser
+	get2:=user.getuser
+	fmt.Printf("%T,%T\n",get1,get2)
+	get2(u,"dayu")
+	fv:=u.getuser
+	fmt.Printf("%T\n",fv)
+}
